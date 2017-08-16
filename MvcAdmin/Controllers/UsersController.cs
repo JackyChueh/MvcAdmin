9b806b34135e670d442e943e1245fdc7619ebe2c
JackyChueh <jackychueh@gmail.com>
2017-08-16 11:28:49 +0800

    Update UsersController.cs

--- MvcAdmin/Controllers/UsersController.cs
+++ MvcAdmin/Controllers/UsersController.cs
@@ -14,7 +14,7 @@ namespace MvcAdmin.Controllers
 
         public ActionResult Index()
         {
-            System.Threading.Thread.Sleep(2000);
+            System.Threading.Thread.Sleep(3000);
             return View(new UsersImplement("SC").GetAll());
         }
 
